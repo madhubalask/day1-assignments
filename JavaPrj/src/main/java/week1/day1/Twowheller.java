72a514651183cef12425027908f728d0f9a67c7a
madhubalask <115484775+madhubalask@users.noreply.github.com>
2022-10-11 09:37:40 +0530

    Add files via upload

---
+++ JavaPrj/src/main/java/week1/day1/Twowheller.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Twowheller {
+	public static void main(String[] args) {
+	int noOfWheels=2;
+	System.out.println(noOfWheels);
+	 short noOfMirrors=2;
+	 System.out.println(noOfMirrors);
+	 long chassisNumber=45678L;
+	 System.out.println(chassisNumber);
+	 boolean isPunctured = false;
+	 System.out.println(isPunctured);
+	 double bikeKM=7509.8;
+	 System.out.println(bikeKM);
+	 String bikeName= "r15 v3";
+	 System.out.println(bikeName);
+	 
+		}
+	}
---
+++ JavaPrj/src/main/java/week1/day1/mobilephone.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class mobilephone {
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	String mobileBrandName= "oppo";
+	System.out.println(mobileBrandName);
+	char mobileLogo=   'O';
+	System.out.println(mobileLogo);
+	short noOfMobilePiece= 10;
+	System.out.println(noOfMobilePiece);
+	int mobileNumber= 2022;
+	System.out.println(mobileNumber);
+	long mobileImeiNumber= 94568989909L;
+	System.out.println(mobileImeiNumber);
+	float mobilePrice=34456.987F;
+	System.out.println(mobilePrice);
+	boolean isDamaged= false;
+	System.out.println(isDamaged);
+
+
+
+		}
+
+	}
+	
+		
+
+
